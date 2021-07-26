5020afff284579b71bccb5f0ef63de37a6831c26
Alburrito <almarlop98@gmail.com>
2021-07-26 14:03:32 +0200

    Day01

---
+++ 2015/Day01/main.py
@@ -0,0 +1,21 @@
+with open('Day01/input', 'r') as f:
+    data = f.read()
+
+
+def get_floor_and_position(data):
+    floor = 0
+    position = -1
+    for index, char in enumerate(data):
+        if char == '(':
+            floor += 1
+        else:
+            floor -= 1
+
+        if position < 0 and floor < 0:
+            position = index
+            
+    return floor,index
+
+
+print(get_floor_and_position(data))
+
